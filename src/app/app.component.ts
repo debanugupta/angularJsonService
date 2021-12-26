ba85b499aa45cb0b37a0d7bad2b82bf36cb60b32
Debanu Gupta <debanugupta@gmail.com>
2021-12-26 19:53:25 +0530

    First Commit with initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angularJsonService';
+export class AppComponent implements OnInit {
+  title = 'app';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
 }
